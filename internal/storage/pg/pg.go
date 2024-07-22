69be6c52e917d025ec78b0221b6df6148841324e
Ilya-SurkoV <lsurkovl@mail.ru>
2024-07-22 21:04:54 +0300

    add: postgres

--- internal/storage/pg/pg.go
+++ internal/storage/pg/pg.go
@@ -1,1 +1,13 @@
 package pg
+
+import "github.com/SurkovIlya/message-handler-app/pkg/postgres"
+
+type PostgresStorage struct {
+	storage *postgres.Database
+}
+
+func New(storage *postgres.Database) *PostgresStorage {
+	return &PostgresStorage{
+		storage: storage,
+	}
+}
